e1f58dd5c435cbbce025a0f209c3fecbaa63de91
NavarroPablo96 <89087992+NavarroPablo96@users.noreply.github.com>
2021-09-12 19:52:41 -0300

    hasta inciso b

--- Nueva carpeta/TrabajoIntegrado1/main.c
+++ Nueva carpeta/TrabajoIntegrado1/main.c
@@ -16,6 +16,8 @@ void AgregaPalabra(TLista *L, char pal[]);
 void MuestraLista(TLista L);
 void MuestraCantidadInformacion(TLista L,int total);
 
+void CargaMatriz(float mat[][4]);
+int valor(char simbolo[10]);
 int main()
 {
     TLista Palabras5=NULL,Palabras7=NULL,Palabras9=NULL;
@@ -35,24 +37,106 @@ int main()
 
     MuestraCantidadInformacion(Palabras5,cantPal5);
 
+    //---Inciso B------
+    printf("\n---Inciso B------");
+    float mat [4][4]={{0,0,0,0},
+                    {0,0,0,0},
+                    {0,0,0,0},
+                    {0,0,0,0}};
+
+
+
+    CargaMatriz(mat);
+    int cantidadTotal=0;
+    for(int i=0; i<4; i++){
+        printf("\n");
+        for(int j=0; j<4; j++){
+            cantidadTotal+=mat[i][j];
+            printf("\t%8.6f",mat[i][j]);
+        }
+    }
+    printf("total= %d",cantidadTotal);
+
     return 0;
 }
 
-void MuestraCantidadInformacion(TLista L,int total){
-    double prob;
-    int aux;
-    while(L!=NULL){
-        //prob=(L->cantApariciones)/total;.
-        aux=L->cantApariciones;
-        printf("\n%d / %d",(aux),total);
-        prob=(aux/total);
-        printf("\n%10.8lf",prob);
-        //printf("\n I(%s)=%f",L->palabra, L->cantApariciones);
+int valor(char simbolo[10]){
+    if(strcmp(simbolo,"00")==0)
+        return 0;
+    else if(strcmp(simbolo,"01")==0)
+        return 1;
+    else if(strcmp(simbolo,"10")==0)
+        return 2;
+    else if(strcmp(simbolo,"11")==0)
+        return 3;
 
+    return 45;
+}
+
+
+
+void CargaMatriz(float mat[][4]){
+    char aux;
+    char simbolo[10];
+    strcpy(simbolo,"");
+    int i=-1,ant,act;
+    int cantCaracteres=2;
+    float cantidadesTotales[4]={0,0,0,0};
+
+    FILE *archt;
+    archt=fopen("datos para tp1.txt","rt");
+
+    if(archt==NULL){
+        printf("No existe el archivo");
+    }
+    else{
+        fscanf(archt,"%c",&aux);
+        simbolo[0]=aux;
+        fscanf(archt,"%c",&aux);
+        simbolo[1]=aux;
+        simbolo[2]='\0';
+        ant=valor(simbolo);
+
+        while(fscanf(archt,"%c",&aux)==1){
+            i++;
+            //printf("\n%c",aux);
+            if(i==(cantCaracteres)){
+                simbolo[cantCaracteres]='\0';
+                act=valor(simbolo);
+                mat[act][ant]++;            //ant es j
+                cantidadesTotales[ant]++;   //act es i
+                ant=act;
+                i=0;
+            }
+            simbolo[i]=aux;
+        }
+
+        simbolo[cantCaracteres]='\0';
+        act=valor(simbolo);
+        mat[act][ant]++;
+        cantidadesTotales[ant]++;
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                mat[j][i] /= cantidadesTotales[i];
+            }
+        }
+    }
+}
+
+void MuestraCantidadInformacion(TLista L,int total){
+    float prob,entropia=0;
+    while(L!=NULL){
+        prob=(float)(L->cantApariciones)/(float)total;
+        printf("\n\nP(%s)=%f",L->palabra,prob);
+        //printf("\n%d",L->cantApariciones);
+        printf("\nI(%s)=%f",L->palabra, (-log(prob)/log(2)) );
+        entropia+= (prob * (-log(prob)/log(2)));
         L=L->sig;
     }
-
+    printf("\nentropia=%f",entropia);
 }
+
 void MuestraLista(TLista L){
     int total=0;
     while(L!=NULL){
